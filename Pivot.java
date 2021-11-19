93bc95df1ff24c883c59afab48c90ccee547890d
Luis Gonzalez <luisgnzhdz@gmail.com>
2021-11-18 22:37:58 -0600

    Create class Pivot

---
+++ Pivot.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Pivot {
+    //Create Scanner's Method
+	private static Scanner entry = new Scanner(System.in);
+
+    //In this class we have to search the pivot value 
+}
