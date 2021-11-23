30368d50931e82a579b57d692d5551e4ff4495fc
Luis Gonzalez <luisgnzhdz@gmail.com>
2021-11-23 13:34:27 -0600

    Add authors on branch operations

--- Project Java/Pivot.java
+++ Project Java/Pivot.java
@@ -1,5 +1,18 @@
 import java.util.*;
 
+/**
+	* Class  
+	*@author Angeles Loredo Alfredo
+	*@author Contreras Ventura Francisco Javier
+	*@author Feregrino Cruz Marlen Aili
+	*@author Fragoso García Sandra
+	*@author González Hernández Luis Ángel
+	*@author Maldonado Santiago Elisa Viridiana
+	*@author Mercado Reyes Monserrat
+    *@date: 10/11/2021
+	*@version 
+*/
+
 public class Pivot {
     //Create Scanner's Method
 	private static Scanner entry = new Scanner(System.in);
